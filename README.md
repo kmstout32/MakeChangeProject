62128f9114fe89e7aa1a8ce3aa7479af47517fa2
Kira Stout <kmstout08@gmail.com>
2023-01-13 19:04:21 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MakeChangeProject
+
+# Desription- 
+Overview of how the program works
+
+# Technologies Used
+
+Java, Eclipse
+
+# Lessons learned 
+
+what I learned while creating this project
---
+++ src/app/MakeChange.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.util.Scanner;
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		
+		    int purchasePrice;
+		    int moneyGiven;
+		    int change;
+		    Scanner sc = new Scanner(System.in);
+		    
+		    //
+		    
+		    System.out.println();
+	}
+
+}
