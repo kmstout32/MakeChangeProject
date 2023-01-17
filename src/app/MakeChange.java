188e29e9dab27383eae0b11b3cf2197fe0c191f9
Kira Stout <kmstout08@gmail.com>
2023-01-16 20:39:27 -0500

    working change depenser

--- src/app/MakeChange.java
+++ src/app/MakeChange.java
@@ -24,41 +24,41 @@ public class MakeChange {
 		boolean isSufficient = true;
 		isSufficient = moneyGiven >= itemPrice;
 		double changeGiven = moneyGiven - itemPrice;
-	
-		
-		
-		// display a default if money given was exact amount, too much or too little.
 
+		// display a default if money given was exact amount, too much or too little.
+//total = 16.73 -- moneyGiven = 20 --changeGiven = 3.27 
+		//16.73*100= 1673 --
+		//1673/1000
 		if (isSufficient) {
 			System.out.println("Payment Accepted! ");
 			System.out.printf("TOTAL CHANGE: $  %.2f", changeGiven);
+			changeGiven = changeGiven * 100;
+			int twenties = (int) changeGiven / 2000;
+			int ten = (int) changeGiven % 2000 / 1000;
+			int five = (int) changeGiven % 1000 / 500;
+			int one = (int) changeGiven % 500 / 100;
+			int quarter = (int) changeGiven % 100 / 25;
+			int dime = (int) changeGiven % 25 / 10;
+			int nickel = (int) changeGiven % 10;
+			int penny = (int) (changeGiven % 5) / 1;
+			System.out.println(changeGiven);
+			System.out.println(changeGiven);
 			
-		//call the method for change amount given
-			//sending moneyGiven, itemPrice, and changeGiven into new method
+			
+			// call the method for change amount given
+			// sending moneyGiven, itemPrice, and changeGiven into new method
 		} else {
 			System.out.println("Insufficient Funds for Transaction.");
 			System.out.printf("Customer owes: $ %.2f ", changeGiven * -1); // -1 in the statement bc I got a negative
 																			// here
 		}
 	}
-}
+
 //		Calculate the amount of change given back to the customer in:
 //		$20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c
 
-//	public static void changeTotal(double moneyGiven, double change) {
-//
-//		if (moneyGiven < change) {
-//			int dollarGiven = (int) change; // how do I separate coins??
-//			int coinGiven = (int) change - dollarGiven;
-//			double changeTotal = 0.0;
-//			
-//			double twenties = (int) dollarGiven / 20;
-//			double ten = (int) dollarGiven / 10;
-//			double five = (int) dollarGiven / 5;
-//			double one = (int) dollarGiven / 1;
-//			double quarter = (int) coinGiven / 25;
-//			double dime = (int) coinGiven / 10;
-//			double nickel = (int) coinGiven / 5;
-//			double penny = (int) (coinGiven % 5) / 1;
-//			return=
+	
 
+		
+}
+//			
